5932e5d31462322d65964ff88e7d329087367d98
Neevs1 <neevan.redkar@gmail.com>
2024-10-05 18:32:57 +0530

    Minor changes

--- sorter.py
+++ sorter.py
@@ -17,8 +17,10 @@ window = Tk() #creates a window using Kinter
 window.title = "Sort according to Branch" #title of window
 button = Button(text="Select file",command=selectFile) #button that executes a command on press
 text=Label(window,text="Please select the required Excel file")
-text.pack()
+title.pack() #added title to window
+text.pack() #aaded text to window
 button.pack() #added button to window
+
 window.geometry("400x400") #set size of window to 400 by 400 px
 window.mainloop() #added window to loop
 
